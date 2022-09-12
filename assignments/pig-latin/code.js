192f4107da3cb337b05ab231fb890a4badb50f7c
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-09-12 15:15:49 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -32,5 +32,6 @@
 // Feel free to play around in the REPL to make sure you understand how this
 // operator works.
 const pigLatin= (word)=> {
-  return word.search (/[aeiou]/)
+  let i = word.search (/[aeiou]/);
+  return word.substring(i);
 }
