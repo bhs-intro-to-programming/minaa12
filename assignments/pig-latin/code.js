b60ca9a5def1d17693bfbcf04b4c6366c7def321
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-09-13 15:31:32 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -37,5 +37,5 @@ const pigLatin= (word)=> {
 }
 const advancedPigLatin= (word)=> {
   i = word.search (/[aeiou]/);
-  return i === 0 ? word+'way' : word.substring(i)+word.substring(0,i)+'ay'
+  return i === 0 ? word+'way' : word.substring(i,word.length)+word.substring(0,i)+'ay'
 }
