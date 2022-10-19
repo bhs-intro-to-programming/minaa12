e2290a25a03db9790c484c8236a8a50c9dc0425e
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-10-19 15:17:46 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -19,8 +19,8 @@ const evens = (arrayofnumbers) => {
   for (let i = 0; i < arrayofnumbers.length; i++) {
     if(arrayofnumbers[i] % 2 === 0 ){
   e.push[arrayofnumbers[i]]
-  i++
     }
+    i++
   }
   return e
 }
