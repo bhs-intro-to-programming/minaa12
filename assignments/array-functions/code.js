52724d7312fa05283d66eadd0dafd078d3a7e3ad
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-10-18 22:21:56 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -7,3 +7,10 @@ const countTens = (arrayofnumbers) => {
   }
   return ntens
 }
+const sum = (arrayofnumbers) => {
+  let s = 0 
+  for (let i = 0; i < arrayofnumbers.length; i++) {
+    s = s+ arrayofnumbers[i]
+  }
+  return s
+}
