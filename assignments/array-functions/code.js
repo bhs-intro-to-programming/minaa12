752e77782ff7cbb9eef5ce24eef09257b8e25294
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-10-20 15:02:04 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,36 +1,33 @@
 const countTens = (arrayofnumbers) => {
   let ntens = 0
   for (let i = 0; i < arrayofnumbers.length; i++) {
-   if(arrayofnumbers[i] === 10) {
-     ntens++
-   }
+    if (arrayofnumbers[i] === 10) {
+      ntens++
+    }
   }
   return ntens
 }
 const sum = (arrayofnumbers) => {
-  let s = 0 
+  let s = 0
   for (let i = 0; i < arrayofnumbers.length; i++) {
-    s = s+ arrayofnumbers[i]
+    s = s + arrayofnumbers[i]
   }
   return s
 }
 const evens = (arrayofnumbers) => {
   let e = []
   for (let i = 0; i < arrayofnumbers.length; i++) {
-    if(arrayofnumbers[i] % 2 === 0 ){
-  e.push(arrayofnumbers[i])
+    if (arrayofnumbers[i] % 2 === 0) {
+      e.push(arrayofnumbers[i])
     }
   }
   return e
 }
 const anyOverOneHundred = (arrayofnumbers) => {
-  if (arrayofnumbers.length>0) {
   for (let i = 0; i < arrayofnumbers.length; i++) {
-    if (arrayofnumbers [i] > 100) {
+    if (arrayofnumbers[i] > 100) {
       return true
-     } else {
-       return false
-     }
     }
+    return false
+
   }
-}
