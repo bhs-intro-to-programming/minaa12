a919854d40cba55b43b5cf90b2810579a5675b23
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-10-20 22:09:13 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -35,7 +35,7 @@ const anyOverOneHundred = (arrayofnumbers) => {
 const pyramid = (p) => {
   let a = []
   for (let i = 1; i <= p; i++) {
-    for (let j = 0; j < 1; j++) {
+    for (let j = 0; j < 2; j++) {
           a.push(i)
     }
   }
