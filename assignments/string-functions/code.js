4a339deb71c5ab449ce657bb4ffef6b05df00f72
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-11-06 20:30:02 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,5 +1,10 @@
+
 const isAllUpperCase = (string) => {
-  
+
+  const isLowerCase = (character) => {
+    return character >= 'a' && character <= 'z'
+  }
+
   for (let i = 0; i < string.length;i++) {
    if (string[i].isLowerCase() ) {
      return false
