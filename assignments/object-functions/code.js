3c91e5a47cd9189feb438f855567f4739769dc1e
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-11-08 18:30:57 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -1,7 +1,3 @@
-let rectange = {
-  width : 10,
-  length : 20
-}
 
 const getX = (object) => {
   return object.x
@@ -17,6 +13,11 @@ const emptyObject = () => {
 
 const distance = (o1,o2) => {
   return Math.sqrt ((o2.x - o1.x)**2 + (o2.y - o1.y)**2)
+}
+
+let rectangle = {
+  width : 10,
+  length : 20
 }
 
 console.log(rectangle.lenght)
