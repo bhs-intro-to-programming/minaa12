0f8ba58963bae15252e16e53e6a3059095ebfa18
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-11-09 14:56:42 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -15,29 +15,3 @@ const distance = (o1,o2) => {
   return Math.sqrt ((o2.x - o1.x)**2 + (o2.y - o1.y)**2)
 }
 
-let rectangle = {
-  width : 10,
-  length : 20
-}
-
-const area = (r) => {
-  return r.width * r.length
-}
-const joke = (o) => {
-  return o.width**2
-}
-let rectangle2 = {
-  width : 100,
-  length : 100
-}
-
-console.log(area(rectangle2))
-
-const largerR = (o1, o2) => {
-  let a1 = area(o1)
-  let a2 = area(o2)
-    if (a1>a2) return a1
-    else return a2
-}
-
-console.log(largerR(rectangle2,rectangle))
