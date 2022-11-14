a10bc169556fd04d39593c790092e6dd51df7a5b
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-11-14 15:26:38 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -2,30 +2,39 @@
 // should save you some time.
 
 const area = (rect) => {
+  return rectangle.width * rectangle.height
 };
 
 const higherPaid = (e1, e2) => {
+if (e1.salary > e2.salary) return e1 
+else return e2
 };
 
 const isSamePoint = (p1, p2) => {
+  if (p1 === p2) return true 
 };
 
 const totalWithTip = (bill, tipPercentage) => {
+  return {subtotal: bill, tip: tipPercentage + subtotal, total: tip}
 };
 
 const isWinner = (player) => {
+  if (player.score > 100) return true 
 };
 
 const updateWins = (players) => {
+
 };
 
 const bigWinners = (players) => {
+
 };
 
 const fillTimesTable = (table) => {
 };
 
 const sums = (n) => {
+  
 };
 
 const rule110 = (cells) => {
