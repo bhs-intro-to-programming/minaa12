809ae2472306e3850d3a06dc97d664ed084d821d
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-11-29 14:54:34 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -3,5 +3,8 @@ const totalEggs = (hard, soft) => {
 }
 
 const chocolatesPerPerson = (chocolate, people) => {
-  return chocolate%people
+  return (chocolate % people)
+}
+const extraChocolates = (chocolates, people) => {
+
 }
