34fbdc25e7b1b85dc31a4e164ccc1d4df05e3d35
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-12-02 14:52:29 -0800

    Updating assessments/2022-12-02-booleans/code.js

---
+++ assessments/2022-12-02-booleans/code.js
@@ -0,0 +1,27 @@
+const needHeavyCoat = (outside, cold) => {
+  return outside && cold
+}
+
+const needSunscreen = (beach, skiing) => {
+  return beach || skiing
+}
+
+const needMittens = (outside, warm) => {
+  return outside && !warm 
+}
+
+const isVenomous = (striped, blue) => {
+  return 
+}
+
+const okaySpeed = (sl, speed) => {
+  if (speed < sl) return true 
+}
+
+const twiceAsExpensive = (p1, p2) => {
+  if (p1 > p2 * 2 ) return true 
+}
+
+const winningRecord = (win, loss, tie) => {
+  if 
+}
