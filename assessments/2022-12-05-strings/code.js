53f79a01c946ec1ff2dbef158041bbf7238c6fdf
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-12-06 14:52:04 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -23,3 +23,7 @@ const containsX = (s) => {
 const slug = (s1, s2, s3) => {
   return s1.toLowerCase() + ('-') + s2.toLowerCase() + ('-') + s3.toLowerCase() 
 }
+
+const capitalize = (s) => {
+  return s[0].toUpperCase + s(0, s.length).toLowerCase
+}
