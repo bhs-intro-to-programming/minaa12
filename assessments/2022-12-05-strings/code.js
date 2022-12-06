d60659425eccbe08a8323eb3fdf8f3fe4e073eda
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-12-06 15:09:49 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -7,7 +7,7 @@ const charactersAround = (s, i) => {
 }
 
 const middle = (s) => {
-  return s.slice(s.length/2) + s.slice(s.length/2)
+  return s.slice(s.length/2, s.length/2 + 1) 
 }
 
 const pair = (s1, s2) => {
