89aa9e2b00f81dad1bc8b8e34d8aa2e374c94f5a
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-12-11 20:27:23 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -54,7 +54,15 @@ for(let i = 0; i < players.length; i++) {
 return m
 };
 
+//has matrix argument
+//starts with all 0's
+//replace each 0 with i * j 
 const fillTimesTable = (table) => {
+for (let i = 0; i < table.length; i++ ) {
+  for (let j = 0; j < table.length; j++) {
+    table[i][j] = (i + 1) * (j + 1)
+  }
+}
 };
 
 const sums = (n) => {
