787a8e1c165e148e1173c64700ed4d67738d27fe
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-12-12 14:57:18 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -1,7 +1,11 @@
 const makeRow = () => {
   let s = []
-  for(let i = 0;  i < 4; i++) {
+  for(let i = 0;  i < 3; i++) {
     s.push([''])
   }
   return s
+}
+
+const makeBoard = () => {
+  let m = []
 }
