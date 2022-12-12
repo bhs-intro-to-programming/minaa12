56f4ee993721a27f778326fe50107f6588ea9927
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-12-12 15:17:05 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -1,20 +1,20 @@
 const makeRow = () => {
   let s = []
-  for(let i = 0;  i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
     s.push('')
   }
   return s
 }
 
 const makeBoard = () => {
-  
+
 }
 
 // s specifies 'X' or 'O'
 // r specifies a row
 // c specifies a column 
-const makeMove = (s, r, c ) => {
-  return {mark: s, row: r, column: c}
+const makeMove = (s, r, c) => {
+  return { mark: s, row: r, column: c }
 }
 
 const placeMark = () => {
@@ -22,8 +22,9 @@ const placeMark = () => {
 }
 
 const allTheSame = (a) => {
-  for(let i = 0; i < a.length; i++) {
-    if (a[0] === a[1] && a[0] === a[2]) { return true
+  for (let i = 0; i < a.length; i++) {
+    if (a[0] === a[1] && a[0] === a[2]) {
+      return true
     }
     else return false
   }
@@ -34,9 +35,11 @@ const extractColumn = () => {
 }
 
 const recordMove = (array, objm) => {
-  let t = [] 
-  for(let i = 0; i < array.length; i++) {
+  let t = []
+  for (let i = 0; i < array.length; i++) {
     array.push(objm)
   }
   return t
 }
+let moves = [];
+recordMove(moves, makeMove('X', 1, 2));
