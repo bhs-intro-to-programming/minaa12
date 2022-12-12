c87d1f396e14b2f51ccc1b0d352dda0fd127e8d5
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-12-11 20:42:06 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -66,7 +66,11 @@ for (let i = 0; i < table.length; i++ ) {
 };
 
 const sums = (n) => {
-  
+  let r = [0]
+  for (let i = 1; i < n + 1; i++) {
+   r[i] = r[i - 1] + i
+  }
+  return r 
 };
 
 const rule110 = (cells) => {
