2fa3f1abb57bfbdd7efa334048c0cc1347ef8ec2
minaa12 <111394931+minaa12@users.noreply.github.com>
2022-12-16 10:59:12 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -1,3 +1,11 @@
 const biggestNumber = (digits) => {
   return 10 ** digits - 1 
+} 
+
+const totalEggs = (hard, soft) => {
+  return hard + soft
+}
+
+const chocolatesPerPerson = (c, p) => {
+  return Math.floor(c/p)
 }
